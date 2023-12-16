37c7c6f9e5e68646095155c4d071fadc9e39ccb9
OlenaKovalenko <elenkow17@gmail.com>
2023-12-16 12:11:05 +0200

    persist

---
+++ src/redux/todosSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todosSlice = createSlice({
+    name: "todos",
+        initialState: {
+            items: [],
+    },
+    reducers: {
+        addTodo(state, { payload }) {
+            state.items.push(payload);
+    },
+        deleteTodo(state, { payload }) {
+            state.items = state.items.filter(item => item.id !== payload);
+    },
+    },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export const todosReducer = todosSlice.reducer;
